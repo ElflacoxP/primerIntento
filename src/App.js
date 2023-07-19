a3a455078bbfbe0cf741f305d055101f62b766ff
ElflacoxP <leorodoni25@gmail.com>
2023-07-18 21:28:30 -0300

    funciona todavia

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 
-
 const App = () => (
 <div className='container'>
-<h1>Hello world!!</h1>
+<h1>Pan con jamon!!</h1>
 </div>
 )
 export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.scss'
 import 'bootstrap'
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+document.body.innerHTML = '<div id="app"></div>'
+
+const root = createRoot(document.getElementById('app'))
+root.render(<App />)
